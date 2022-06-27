f4d6af806b22d2daf295688a66f9d8ec3dda2057
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-06-27 22:26:53 +0300

    Changed the project's folder.

---
+++ 01.Inheritance/02.ReusingClasses/CustomRandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        Random rand = new Random();
+
+        public string RandomString()
+        {
+            if (Count == 0) return null;
+
+            int index = rand.Next(0, this.Count);
+            string element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
---
+++ 01.Inheritance/README.md
@@ -0,0 +1,2 @@
+# CSharp-OOP
+This repository contains the solutions I have made as practice for the SoftUni CSharp Object-oriented programming Module.
