62a14f8a34b0c4144ce3194354f1cf83c0e63ce1
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-06-29 14:33:36 +0300

    Commited a new Inheritance exercise.

---
+++ 01.Inheritance/Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name,int age)
+        {
+       //     if (age <= 0) throw new Exception("People cannot have age under 0 years old.");
+            this.name = name;
+            this.age = age;
+
+        }
+        public string Name => this.name;
+        public int Age => this.age;
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {age}";
+
+        }
+    }
+}
